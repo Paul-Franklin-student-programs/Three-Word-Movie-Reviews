916c995f1ecf06bf545ae94b332416f5aea4506e
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2025-02-24 14:45:23 -0500

    cleaned up JSON, updated javascript logic for more accurate table sorting

--- js/movieReviews.js
+++ js/movieReviews.js
@@ -84,10 +84,28 @@ function toggleSort(column, clickedHeader) {
         currentSortOrder = "asc";
     }
 
+    const gradeValues = {
+        "A+": 13, "A": 12, "A-": 11,
+        "B+": 10, "B": 9, "B-": 8,
+        "C+": 7, "C": 6, "C-": 5,
+        "D+": 4, "D": 3, "D-": 2,
+        "F": 1
+    };
+
     // Sort movie data
     movieData.sort((a, b) => {
         let valA = a[column] || "";
         let valB = b[column] || "";
+
+        if (column === "letter grade") {
+            valA = gradeValues[valA];
+            valB = gradeValues[valB];
+        }
+
+          if (column === "rotten tomatoes score") {
+              valA = valA.replace("%", "");
+              valB = valB.replace("%", "");
+          }
 
         // Convert numbers for numerical sorting
         if (!isNaN(valA) && !isNaN(valB)) {
